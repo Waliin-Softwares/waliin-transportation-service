3cbf5105559960e6b8dcf1deb18253353f72e0a5
JONNY-ME <yohannesmelese4@gmail.com>
2022-01-06 15:22:11 +0300

    [add] update users table

--- migrations/m_001.php
+++ migrations/m_001.php
@@ -7,9 +7,15 @@ class m_001{
             $db = Application::$app->db;
             $SQL =  "CREATE TABLE `users` (
                 `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-                `name` varchar(255) NOT NULL,
+                `username` varchar(255) NOT NULL,
                 `email` varchar(255) NOT NULL,
+                `firstName` varchar(255) NOT NULL,
+                `lastName` varchar(255) NOT NULL,
                 `password` varchar(255) NOT NULL,
+                `address` varchar(255) NOT NULL,
+                `dateOfBirth` Date,
+                `gender` char(1),
+                `role` varchar(255) NOT NULL,
                 `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
                 `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
             ) ENGINE=InnoDB DEFAULT CHARSET=utf8;";
